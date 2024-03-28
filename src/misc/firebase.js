5f878d0561282c33ba06121379df9cb58eb1b935
Pranav Mange <pranavmange2015@gmail.com>
2024-03-28 18:17:31 +0530

    facebook and google added

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCmtg_lfTV7dHDuUCcsLEoCigQ1AI8gYu8',
+  authDomain: 'chat-web-app-68ec2.firebaseapp.com',
+  projectId: 'chat-web-app-68ec2',
+  storageBucket: 'chat-web-app-68ec2.appspot.com',
+  messagingSenderId: '979997548470',
+  appId: '1:979997548470:web:54a6f87c323d36dfefab17',
+};
+
+const app = firebase.initializeApp(config);
