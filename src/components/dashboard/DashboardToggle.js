5896c299950e3264358387318a28e4b3241c1f0f
Pranav Mange <pranavmange2015@gmail.com>
2024-03-31 23:51:31 +0530

    dashboard added

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Button, Icon } from 'rsuite';
+import { Button, Drawer, Icon } from 'rsuite';
+import { useModelState } from '../../misc/custom-hooks';
+import Dashboard from '.';
 
 const DashboardToggle = () => {
+  const { isOpen, close, open } = useModelState();
   return (
     <>
-      <Button block color="blue">
+      <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
+      <Drawer show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
     </>
   );
 };
