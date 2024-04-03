a53ae7687b326748e212602560cbc787a28f78c2
Pranav Mange <pranavmange2015@gmail.com>
2024-04-03 14:00:53 +0530

    chatrooms added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,26 @@
-import React from 'react';
 import DashboardToggle from './dashboard/DashboardToggle';
+import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
+import { Divider } from 'rsuite';
+import ChatRoomList from './rooms/ChatRoomList';
+import { useEffect, useRef, useState } from 'react';
 const Sidebar = () => {
+  const topSidebarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>
         <DashboardToggle />
+        <CreateRoomBtnModal />
+        <Divider>Join Conversation </Divider>
       </div>
-      bottom
+      <ChatRoomList aboveElHeight={height} />
     </div>
   );
 };
